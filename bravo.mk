a1e0829b2ccd89844aae5c31464fce339b9cd254
Koushik Dutta <koushd@gmail.com>
2010-08-25 17:18:29 -0700

    Fix up the bravo product to not set the kernel for EVERY product.

--- bravo.mk
+++ bravo.mk
@@ -19,13 +19,6 @@
 # not specialized for any geography.
 #
 
-# Kernel Targets
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-ifeq ($(TARGET_KERNEL_CONFIG),)
-TARGET_PREBUILT_KERNEL := device/htc/bravo/kernel/kernel
-endif # TARGET_KERNEL_CONFIG
-endif # TARGET_PREBUILT_KERNEL
-
 ## (1) First, the most specific values, i.e. the aspects that are specific to GSM
 
 PRODUCT_COPY_FILES += \
@@ -158,7 +151,14 @@ PRODUCT_COPY_FILES += \
     device/htc/bravo/kernel/2.6.34.5-cyanogenmod/modules.pcimap:system/lib/modules/2.6.34.5-cyanogenmod/modules.pcimap \
     device/htc/bravo/kernel/2.6.34.5-cyanogenmod/modules.inputmap:system/lib/modules/2.6.34.5-cyanogenmod/modules.inputmap
 
-PRODUCT_COPY_FILES += $(TARGET_PREBUILT_KERNEL):kernel
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := device/htc/bravo/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+	$(LOCAL_KERNEL):kernel
 
 # stuff common to all HTC phones
 $(call inherit-product, device/htc/common/common.mk)
