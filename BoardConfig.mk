5d640f26d8b17df65cc7e4b59177ce7b97f0891f
Koushik Dutta <koushd@gmail.com>
2010-11-11 00:05:26 -0800

    Remove reference to updater lib.
    Change-Id: I2f5bb66432127305646c2fc8dd8305075a4f115e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,7 +83,6 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 
 TARGET_RECOVERY_UI_LIB := librecovery_ui_bravo librecovery_ui_htc
 
-TARGET_RECOVERY_UPDATER_LIBS += librecovery_updater_htc
 
 TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
 TARGET_PREBUILT_KERNEL := device/htc/bravo/kernel/kernel
