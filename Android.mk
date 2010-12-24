4fb07ed6ed56d01ae53c2d5c9a27b8e768b6912f
Steve Kondik <shade@chemlab.org>
2010-12-24 12:42:14 -0500

    bravo: Remove recovery from makefile
    Change-Id: I4f540d05f4f4f3b77cfafc5cf6655132a9a42d20

--- Android.mk
+++ Android.mk
@@ -15,7 +15,6 @@
 LOCAL_PATH := $(my-dir)
 subdir_makefiles := \
 	$(LOCAL_PATH)/libsensors/Android.mk \
-	$(LOCAL_PATH)/liblights/Android.mk \
-	$(LOCAL_PATH)/recovery/Android.mk
+	$(LOCAL_PATH)/liblights/Android.mk
 
 include $(subdir_makefiles)
