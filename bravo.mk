15d960cd94c53ac4da708b71d49db79f1958678f
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2011-02-09 11:37:42 +0000

    Add distinct multitouch capability
    I want my Google Maps finger-rotation! :)
    
    Change-Id: Ifefdab2993cb1e9e94d2056fbb108ceb093ecc54

--- bravo.mk
+++ bravo.mk
@@ -80,7 +80,8 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
     frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
     frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml
 
 # media config xml file
 PRODUCT_COPY_FILES += \
