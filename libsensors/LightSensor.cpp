c380f9872dfc33c4c4177fc85acfaa977e23cbdf
Scott Brady <sbradymobile@gmail.com>
2011-05-29 21:35:46 -0700

    bravo: Match light sensor levels to board-bravo.c
    Matches light levels from cm-kernel/arch/arm/mach-msm/board-bravo.c
    to libsensors/LightSensor.cpp and overlay/frameworks/base/core/res/res/values/config.xml.
    
    Let me know if the brightness values need to change.
    
    Change-Id: Ia06d40d479c3fe892ec1b373fd189bfa39d56fe4

--- libsensors/LightSensor.cpp
+++ libsensors/LightSensor.cpp
@@ -143,8 +143,8 @@ int LightSensor::readEvents(sensors_event_t* data, int count)
 float LightSensor::indexToValue(size_t index) const
 {
     static const float luxValues[10] = {
-            10.0, 160.0, 225.0, 320.0, 640.0, 
-            1280.0, 2600.0, 5800.0, 8000.0, 10240.0
+            0.0, 1.0, 15.0, 30.0, 60.0, 
+            289.0, 400.0, 698.0, 860.0, 1023.0
     };
 
     const size_t maxIndex = sizeof(luxValues)/sizeof(*luxValues) - 1;
